7ae5497414da4d56d10a82ed225450168ba43294
Yuan Cao <caoyuan96421@gmail.com>
2018-08-18 23:40:17 -0400

    a

---
+++ PushToGo_Driver/PushToGo_Driver/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+// TODO - Add your authorship information here
+[assembly: AssemblyTitle("ASCOM.PushToGo.Telescope")]
+[assembly: AssemblyDescription("ASCOM Telescope driver for PushToGo")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("The ASCOM Initiative")]
+[assembly: AssemblyProduct("ASCOM Telescope driver for PushToGo")]
+[assembly: AssemblyCopyright("Copyright © 2018 The ASCOM Initiative")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d895bf95-7d9b-4a4d-9c13-66f8b7198d79")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+//
+// TODO - Set your driver's version here
+[assembly: AssemblyVersion("6.4.0.0")]
+[assembly: AssemblyFileVersion("6.4.0.0")]
---
+++ PushToGo_Driver/PushToGo_Driver/Properties/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace ASCOM.PushToGo.Properties
+{
+    [DeviceId("ASCOM.PushToGo.Telescope", DeviceName = "PushToGo Mount")]
+    [SettingsProvider(typeof(ASCOM.SettingsProvider))]
+    internal partial class Settings
+    {
+    }
+}
